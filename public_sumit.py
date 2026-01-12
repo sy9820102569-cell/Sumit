c436f9e3ab982b2b9e6de9e0e0217e25c7c596a1
sy9820102569-cell <sy9820102569@gmail.com>
2026-01-12 13:52:50 +0530

    Create public_sumit.py

---
+++ public_sumit.py
@@ -0,0 +1,10 @@
+# Simple Python Program
+
+name = input("Enter your name: ")
+print("Hello,", name, "👋 Welcome to Python!")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", a + b)
+print("Thank you for using this program 😄")
